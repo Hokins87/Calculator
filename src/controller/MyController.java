ed937e88317025709cf33e042593b6ebe2de2c3d
Hokins87 <hokins87@gmail.com>
2015-12-17 00:07:31 +0200

    version 3

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -16,7 +16,17 @@ public class MyController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		MyEvent btnEvent = new MyEvent();
+	    btn0.setOnAction(btnEvent);
+	    btn1.setOnAction(btnEvent);
+	    btn2.setOnAction(btnEvent);
+	    btn3.setOnAction(btnEvent);
+	    btn4.setOnAction(btnEvent);
+	    btn5.setOnAction(btnEvent);
+	    btn6.setOnAction(btnEvent);
+	    btn7.setOnAction(btnEvent);
+	    btn8.setOnAction(btnEvent);
+	    btn9.setOnAction(btnEvent);
 	}
 
 	private double oper1 = 0, oper2 = 0;
@@ -33,64 +43,6 @@ public class MyController implements Initializable {
 
 	@FXML
 	private AnchorPane lblDisplay;
-
-	public void btnClick(ActionEvent event) {
-		btn0.setOnAction(new MyEvent());
-		btn1.setOnAction(new MyEvent());
-		btn2.setOnAction(new MyEvent());
-		btn3.setOnAction(new MyEvent());
-		btn4.setOnAction(new MyEvent());
-		btn5.setOnAction(new MyEvent());
-		btn6.setOnAction(new MyEvent());
-		btn7.setOnAction(new MyEvent());
-		btn8.setOnAction(new MyEvent());
-		btn9.setOnAction(new MyEvent());
-	}
-	
-	/*
-	 * public void addNum0(ActionEvent event) { 
-	 * if(btn0 == event.getSource()){
-	 * if(txtField.getText().equals("0")){ 
-	 * txtField.setText(txtField.getText() +
-	 * ""); }else{ txtField.setText(txtField.getText() + "0");
-	 * System.out.println("0"); } } }
-	 *
-	 * public void addNum1(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "1"); System.out.println("1"); }
-	 * 
-	 * public void addNum2(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "2"); System.out.println("2"); }
-	 * 
-	 * public void addNum3(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "3"); System.out.println("3"); }
-	 * 
-	 * public void addNum4(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "4"); System.out.println("4"); }
-	 * 
-	 * public void addNum5(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "5"); System.out.println("5"); }
-	 * 
-	 * public void addNum6(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "6"); System.out.println("6"); }
-	 * 
-	 * public void addNum7(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "7"); System.out.println("7"); }
-	 * 
-	 * public void addNum8(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "8"); System.out.println("8"); }
-	 * 
-	 * public void addNum9(ActionEvent event) {
-	 * if(txtField.getText().equals("0")){ txtField.setText(""); }
-	 * txtField.setText(txtField.getText() + "9"); System.out.println("9"); }
-	 */
 
 	public void clearDisplay(ActionEvent event) {
 		System.out.println("Clear");
@@ -160,38 +112,7 @@ public class MyController implements Initializable {
 				txtField.setText("");
 			}
 			txtField.setText(txtField.getText() + btn.getText());
-			switch (btn.getText()) {
-			case "0":
-				System.out.println("0");
-				break;
-			case "1":
-				System.out.println("1");
-				break;
-			case "2":
-				System.out.println("2");
-				break;
-			case "3":
-				System.out.println("3");
-				break;
-			case "4":
-				System.out.println("4");
-				break;
-			case "5":
-				System.out.println("5");
-				break;
-			case "6":
-				System.out.println("6");
-				break;
-			case "7":
-				System.out.println("7");
-				break;
-			case "8":
-				System.out.println("8");
-				break;
-			case "9":
-				System.out.println("9");
-				break;
-			}
+			System.out.println(btn.getText());
 		}
 
 	}
